4dcd0fde268451b0dd18f769a4a726f6e2abfc40
Sania Ahmad <sania.ahmad483@gmail.com>
2023-03-25 16:45:58 +0530

    rules changed

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -21,9 +21,9 @@ export default function Main() {
         <h1 className='title text-light'>Quiz Application</h1>
 
         <ol>
-            <li>You will be asked 10 questions one after another.</li>
-            <li>10 points is awarded for the correct answer.</li>
-            <li>Each question has three options. You can choose only one options.</li>
+            <li>You will be asked 5 questions one after another.</li>
+            <li>10 points are awarded for the correct answer.</li>
+            <li>Each question has three options. You can choose only one option.</li>
             <li>You can review and change answers before the quiz finish.</li>
             <li>The result will be declared at the end of the quiz.</li>
         </ol>
